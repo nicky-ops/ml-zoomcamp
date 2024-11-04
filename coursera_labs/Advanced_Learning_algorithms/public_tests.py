bcef02df4674277bf3a090c4c64d345e1f63b520
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-11-04 10:49:58 +0300

    decision trees

--- coursera_labs/Advanced_Learning_algorithms/public_tests.py
+++ coursera_labs/Advanced_Learning_algorithms/public_tests.py
@@ -1,103 +1,169 @@
-# UNIT TESTS
-from tensorflow.keras.activations import sigmoid as tf_keras_sigmoid
-from tensorflow.keras.layers import Dense
-
 import numpy as np
 
-def test_c1(target):
-    assert len(target.layers) == 3, \
-        f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
-    assert target.input.shape.as_list() == [None, 400], \
-        f"Wrong input shape. Expected [None,  400] but got {target.input.shape.as_list()}"
-    i = 0
-    expected = [[Dense, [None, 25], tf_keras_sigmoid],
-                [Dense, [None, 15], tf_keras_sigmoid],
-                [Dense, [None, 1], tf_keras_sigmoid]]
+def compute_entropy_test(target):
+    y = np.array([1] * 10)
+    result = target(y)
+    
+    assert result == 0, "Entropy must be 0 with array of ones"
+    
+    y = np.array([0] * 10)
+    result = target(y)
+    
+    assert result == 0, "Entropy must be 0 with array of zeros"
+    
+    y = np.array([0] * 12 + [1] * 12)
+    result = target(y)
+    
+    assert result == 1, "Entropy must be 1 with same ammount of ones and zeros"
+    
+    y = np.array([1, 0, 1, 0, 1, 1, 1, 0, 1])
+    assert np.isclose(target(y), 0.918295, atol=1e-6), "Wrong value. Something between 0 and 1"
+    assert np.isclose(target(-y + 1), target(y), atol=1e-6), "Wrong value"
+    
+    print("\033[92m All tests passed. ")
 
-    for layer in target.layers:
-        assert type(layer) == expected[i][0], \
-            f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
-        assert layer.output.shape.as_list() == expected[i][1], \
-            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
-        assert layer.activation == expected[i][2], \
-            f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
-        i = i + 1
+def split_dataset_test(target):
 
-    print("\033[92mAll tests passed!")
+    # Case 1
+    X = np.array([[1, 0], 
+         [1, 0], 
+         [1, 1], 
+         [0, 0], 
+         [0, 1]])
+    X_t = np.array([[0, 1, 0, 1, 0]])
+    X = np.concatenate((X, X_t.T), axis=1)
+
+    left, right = target(X, list(range(5)), 2)
+    expected = {'left': np.array([1, 3]),
+                'right': np.array([0, 2, 4])}
+
+    assert type(left) == list, f"Wrong type for left. Expected: list got: {type(left)}"
+    assert type(right) == list, f"Wrong type for right. Expected: list got: {type(right)}"
     
-def test_c2(target):
+    assert type(left[0]) == int, f"Wrong type for elements in the left list. Expected: int got: {type(left[0])}"
+    assert type(right[0]) == int, f"Wrong type for elements in the right list. Expected: number got: {type(right[0])}"
     
-    def linear(a):
-        return a
+    assert len(left) == 2, f"left must have 2 elements but got: {len(left)}"
+    assert len(right) == 3, f"right must have 3 elements but got: {len(right)}"
+
+    assert np.allclose(right, expected['right']), f"Wrong value for right. Expected: { expected['right']} \ngot: {right}"
+    assert np.allclose(left, expected['left']), f"Wrong value for left. Expected: { expected['left']} \ngot: {left}"
+
+
+    # Case 2
+    X = np.array([[0, 1], 
+         [1, 1], 
+         [1, 1], 
+         [0, 0], 
+         [1, 0]])
+    X_t = np.array([[0, 1, 0, 1, 0]])
+    X = np.concatenate((X_t.T, X), axis=1)
+
+    left, right = target(X, list(range(5)), 0)
+    expected = {'left': np.array([1, 3]),
+                'right': np.array([0, 2, 4])}
+
+
+    assert len(left) == 2, f"left must have 2 elements but got: {len(left)}" 
+    assert len(right) == 3, f"right must have 3 elements but got: {len(right)}"
+    assert np.allclose(right, expected['right']) and np.allclose(left, expected['left']), f"Wrong value when target is at index 0."
+
+
+    # Case 3
+    X = (np.random.rand(11, 3) > 0.5) * 1 # Just random binary numbers
+    X_t = np.array([[0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0]])
+    X = np.concatenate((X, X_t.T), axis=1)
+
+    left, right = target(X, [1, 2, 3, 6, 7, 9, 10], 3)
+    expected = {'left': np.array([1, 3, 6]),
+                'right': np.array([2, 7, 9, 10])}
+
+    assert np.allclose(right, expected['right']) and np.allclose(left, expected['left']), f"Wrong value when target is at index 0. \nExpected: {expected} \ngot: \{left:{left}, 'right': {right}\}"
+ 
     
-    def linear_times3(a):
-        return a * 3
+    print("\033[92m All tests passed.")
+
+def compute_information_gain_test(target):
+    X = np.array([[1, 0], 
+         [1, 0], 
+         [1, 0], 
+         [0, 0], 
+         [0, 1]])
     
-    x_tst = np.array([1., 2., 3., 4.])  # (1 examples, 3 features)
-    W_tst = np.array([[1., 2.], [1., 2.], [1., 2.], [1., 2.]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0.])  # (2 features)
+    y = np.array([[0, 0, 0, 0, 0]]).T
+    node_indexes = list(range(5))
+
+    result1 = target(X, y, node_indexes, 0)
+    result2 = target(X, y, node_indexes, 0)
     
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert A_tst.shape[0] == len(b_tst)
-    assert np.allclose(A_tst, [10., 20.]), \
-        "Wrong output. Check the dot product"
+    assert result1 == 0 and result2 == 0, f"Information gain must be 0 when target variable is pure. Got {result1} and {result2}"
     
-    b_tst = np.array([3., 5.])  # (2 features)
+    y = np.array([[0, 1, 0, 1, 0]]).T
+    node_indexes = list(range(5))
     
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [13., 25.]), \
-        "Wrong output. Check the bias term in the formula"
+    result = target(X, y, node_indexes, 0)
+    assert np.isclose(result, 0.019973, atol=1e-6), f"Wrong information gain. Expected {0.019973} got: {result}"
     
-    A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [39., 75.]), \
-        "Wrong output. Did you apply the activation function at the end?"
+    result = target(X, y, node_indexes, 1)
+    assert np.isclose(result, 0.170951, atol=1e-6), f"Wrong information gain. Expected {0.170951} got: {result}"
+
+    node_indexes = list(range(4))
+    result = target(X, y, node_indexes, 0)
+    assert np.isclose(result, 0.311278, atol=1e-6), f"Wrong information gain. Expected {0.311278} got: {result}"
+
+    result = target(X, y, node_indexes, 1)
+    assert np.isclose(result, 0, atol=1e-6), f"Wrong information gain. Expected {0.0} got: {result}"
+
+    print("\033[92m All tests passed.")
+
+def get_best_split_test(target):
+    X = np.array([[1, 0], 
+         [1, 0], 
+         [1, 0], 
+         [0, 0], 
+         [0, 1]])
+
+    y = np.array([[0, 0, 0, 0, 0]]).T
+    node_indexes = list(range(5))
+
+    result = target(X, y, node_indexes)
     
-    print("\033[92mAll tests passed!")  
+    assert result == -1, f"When the target variable is pure, there is no best split to do. Expected -1, got {result}"
     
-def test_c3(target):
+    y = X[:,0]
+    result = target(X, y, node_indexes)
+    assert result == 0, f"If the target is fully correlated with other feature, that feature must be the best split. Expected 0, got {result}"
+    y = X[:,1]
+    result = target(X, y, node_indexes)
+    assert result == 1, f"If the target is fully correlated with other feature, that feature must be the best split. Expected 1, got {result}"
+
+    y = 1 - X[:,0]
+    result = target(X, y, node_indexes)
+    assert result == 0, f"If the target is fully correlated with other feature, that feature must be the best split. Expected 0, got {result}"
+
+    y = np.array([[0, 1, 0, 1, 0]]).T
+    result = target(X, y, node_indexes)
+    assert result == 1, f"Wrong result. Expected 1, got {result}"
+
+    y = np.array([[0, 1, 0, 1, 0]]).T    
+    node_indexes = [2, 3, 4]
+    result = target(X, y, node_indexes)
+    assert result == 0, f"Wrong result. Expected 0, got {result}"
+
+    n_samples = 100
+    X0 = np.array([[1] * n_samples])
+    X1 = np.array([[0] * n_samples])
+    X2 = (np.random.rand(1, 100) > 0.5) * 1
+    X3 = np.array([[1] * int(n_samples / 2) + [0] * int(n_samples / 2)])
     
-    def linear(a):
-        return a
+    y = X2.T
+    node_indexes = list(range(20, 80))
+    X = np.array([X0, X1, X2, X3]).T.reshape(n_samples, 4)
+    result = target(X, y, node_indexes)
     
-    def linear_times3(a):
-        return a * 3
+    assert result == 2, f"Wrong result. Expected 2, got {result}"
     
-    x_tst = np.array([1., 2., 3., 4.])  # (1 examples, 3 features)
-    W_tst = np.array([[1., 2.], [1., 2.], [1., 2.], [1., 2.]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0.])  # (2 features)
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert A_tst.shape[0] == len(b_tst)
-    assert np.allclose(A_tst, [10., 20.]), \
-        "Wrong output. Check the dot product"
-    
-    b_tst = np.array([3., 5.])  # (2 features)
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [13., 25.]), \
-        "Wrong output. Check the bias term in the formula"
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [39., 75.]), \
-        "Wrong output. Did you apply the activation function at the end?"
-    
-    x_tst = np.array([[1., 2., 3., 4.], [5., 6., 7., 8.]])  # (2 examples, 4 features)
-    W_tst = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12]]) # (3 input features, 2 output features)
-    b_tst = np.array([0., 0., 0.])  # (2 features)
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert A_tst.shape == (2, 3)
-    assert np.allclose(A_tst, [[ 70.,  80.,  90.], [158., 184., 210.]]), \
-        "Wrong output. Check the dot product"
-    
-    b_tst = np.array([3., 5., 6])  # (3 features)
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear)
-    assert np.allclose(A_tst, [[ 73.,  85.,  96.], [161., 189., 216.]]), \
-        "Wrong output. Check the bias term in the formula"
-    
-    A_tst = target(x_tst, W_tst, b_tst, linear_times3)
-    assert np.allclose(A_tst, [[ 219.,  255.,  288.], [483., 567., 648.]]), \
-        "Wrong output. Did you apply the activation function at the end?"
-    
-    print("\033[92mAll tests passed!")  
+    y = X0.T
+    result = target(X, y, node_indexes)
+    assert result == -1, f"When the target variable is pure, there is no best split to do. Expected -1, got {result}"
+    print("\033[92m All tests passed.")
